d7df3786b605b6f723a9b557e282fd8bc025bb1d
khrtz <garula.oze498@gmail.com>
2017-04-13 19:10:25 +0900

    change to modern javascript

--- index.jsx
+++ index.jsx
@@ -1,19 +1,34 @@
-'use strict';
+import React from 'react';
+import { render } from 'react-dom';
+import TimePicker from './src/TimePicker';
+import * as moment from "moment";
 
-var React      = require('react')
-var TimePicker = require('./src')
+export default class DemoApp extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 0
+    }
+  }
 
-var VALUE = '14:00:01'
+  onChange(value, moment) {
+    this.setState({
+      value: value
+    })
+  }
 
-var onChange = function(value){
-    // value = value.substring(0, 5)
-    picker.setProps({value: value})
+  render() {
+    return (
+      <TimePicker
+        value = {this.state.value}
+        onChange = {this.onChange.bind(this)}
+      />
+    )
+  }
 }
 
-var picker = React.render(
-    <TimePicker
-        value={VALUE}
-        onChange={onChange}
-    />,
-    document.getElementById('content')
+render(
+  <DemoApp />,
+  document.getElementById('content')
 )
